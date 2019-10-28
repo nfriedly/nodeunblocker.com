19e08ccc9e9375291abeaeb74d54b9efb5f794f2
RajasG <rajasmgandhi@gmail.com>
2019-10-28 18:37:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/***************
- * node-unblocker: Web Proxy for evading firewalls and content filters,
- * similar to CGIProxy or PHProxy
- *
- *
- * This project is hosted on github:  https://github.com/nfriedly/nodeunblocker.com
- *
- * By Nathan Friedly - http://nfriedly.com
- * Released under the terms of the Affero GPL v3
- */
-
 var url = require('url');
 var querystring = require('querystring');
 var express = require('express');
