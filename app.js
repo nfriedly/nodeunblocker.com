d6d82b68d6c9f0d16d5d2d8ed10c3439cb483e9b
RajasG <rajasmgandhi@gmail.com>
2019-10-31 18:07:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,41 +3,12 @@ var querystring = require('querystring');
 var express = require('express');
 var unblocker = require('unblocker');
 var Transform = require('stream').Transform;
-// HTTP Authentication 
-var preAuth = require('http-auth');
-
-var basic = preAuth.basic({
-        realm: "Restricted Access! Please login to proceed"
-    }, function (username, password, callback) { 
-         callback( (username === "user" && password === "password"));
-    }
-);
 
 // Setup server
 var app = express();
-    app.use(preAuth.connect(basic));
 var server = require('http').createServer(app);
 
 var google_analytics_id = process.env.GA_ID || null;
-
-function addGa(html) {
-    if (google_analytics_id) {
-        var ga = [
-            "<script type=\"text/javascript\">",
-            "var _gaq = []; // overwrite the existing one, if any",
-            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
-            "_gaq.push(['_trackPageview']);",
-            "(function() {",
-            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
-            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
-            "})();",
-            "</script>"
-            ].join("\n");
-        html = html.replace("</body>", ga + "\n\n</body>");
-    }
-    return html;
-}
 
 function googleAnalyticsMiddleware(data) {
     if (data.contentType == 'text/html') {
