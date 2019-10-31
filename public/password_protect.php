dd0a9f76be994453312e137398c0f63c9f742cae
RajasG <rajasmgandhi@gmail.com>
2019-10-31 12:17:37 -0500

    Update password_protect.php

--- public/password_protect.php
+++ public/password_protect.php
@@ -49,15 +49,14 @@ $LOGIN_INFORMATION = array(
 // Add login/password pairs below, like described above
 // NOTE: all rows except last must have comma "," at the end of line
 $LOGIN_INFORMATION = array(
-  'zubrag' => 'root',
-  'admin' => 'adminpass'
+  'rmg' => 'yeet',
 );
 
 // request login? true - show login and password boxes, false - password box only
 define('USE_USERNAME', true);
 
 // User will be redirected to this page after logout
-define('LOGOUT_URL', 'http://www.example.com/');
+define('LOGOUT_URL', 'http://www.hwyeet.herokuapp.com/');
 
 // time out after NN minutes of inactivity. Set to 0 to not timeout
 define('TIMEOUT_MINUTES', 0);
