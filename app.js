70490d30fbe872bfb227197c61458eefb7753cd3
RajasG <rajasmgandhi@gmail.com>
2019-11-01 15:42:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var url = require('url');
+/*var url = require('url');
 var querystring = require('querystring');
 var unblocker = require('unblocker');
 var Transform = require('stream').Transform;
@@ -35,11 +35,37 @@ app.get("/no-js", function(req, res) {
     var site = querystring.parse(url.parse(req.url).query).url;
     // and redirect the user to /proxy/url
     res.redirect(unblockerConfig.prefix + site);
-});*/
+});
 
 app
   .use(auth.connect(basic))
   .get('/', (req, res) => res.send(`Hello from express - ${req.user}!`))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));*/
 
-// modules.export = app;
+// modules.export = app;*/
+const express = require('express');
+const auth = require('http-auth');
+
+require('dotenv').config();
+
+const basic = auth.basic({
+        realm: "Private Area"
+    }, (username, password, callback) => {
+        callback(username === process.env.USER_NAME && password === process.env.USER_PASSW);
+    }
+);
+
+basic.on('fail', (result, req) => {
+  console.log(`User authentication failed: ${result.user}`);
+});
+
+basic.on('error', (error, req) => {
+  console.log(`Authentication error: ${error.code + " - " + error.message}`);
+});
+
+const PORT = process.env.PORT || 5000;
+ 
+express()
+  .use(auth.connect(basic))
+  .get('/', (req, res) => res.send(`Hello from express - ${req.user}!`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
