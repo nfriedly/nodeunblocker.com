55c8a7d2e6916350e10e755eac99753378bd3394
RajasG <rajasmgandhi@gmail.com>
2019-10-31 19:56:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,13 +25,15 @@ var unblockerConfig = {
     ]
 };
 
+
+app.use(auth.connect(basic));
+
 // this line must appear before any express.static calls (or anything else that sends responses)
 app.use(unblocker(unblockerConfig));
 
 // serve up static files *after* the proxy is run
 app.use('/', express.static(__dirname + '/public'));
 
-app.use(auth.connect(basic));
 
 // this is for users who's form actually submitted due to JS being disabled or whatever
 app.get("/no-js", function(req, res) {
