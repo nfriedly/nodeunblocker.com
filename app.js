586617feab8ff82524a37ef477888bc61baf35a0
RajasG <rajasmgandhi@gmail.com>
2019-11-01 07:47:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ var express = require('express');
 var unblocker = require('unblocker');
 var Transform = require('stream').Transform;
 
-// Authentication module.
-/*var auth = require('http-auth');
-var basic = auth.basic({
-		realm: "Simon Area."
-	}, (username, password, callback) => { 
-	    // Custom authentication
-	    // Use callback(error) if you want to throw async error.
-		callback(username === "Tina" && password === "Bullock");
-	}
-);*/
-// Setup server
 var app = express();
 var server = require('http').createServer(app);
 
@@ -24,9 +13,6 @@ var unblockerConfig = {
         googleAnalyticsMiddleware
     ]
 };
-
-
-//app.use(auth.connect(basic));
 
 // this line must appear before any express.static calls (or anything else that sends responses)
 app.use(unblocker(unblockerConfig));
