144b429bd9a455ddbee5fdce0e26e426b56d7219
RajasG <rajasmgandhi@gmail.com>
2019-11-03 14:31:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -15,12 +15,10 @@ var basic = auth.basic({
 
 basic.on('fail', (result, req) => {
   console.log(`User authentication failed: ${result.user}`);
-  process.exit();
 });
 
 basic.on('error', (error, req) => {
   console.log(`Authentication error: ${error.code + " - " + error.message}`);
-  process.exit();
 });
 
 const PORT = process.env.PORT || 5000;
@@ -32,19 +30,18 @@ var unblockerConfig = {
 };
 
 
-app.get("/no-js", function(req, res) {
-    // grab the "url" parameter from the querystring
-    var site = querystring.parse(url.parse(req.url).query).url;
-    // and redirect the user to /proxy/url
-    res.redirect(unblockerConfig.prefix + site);
-});
-
 app
   .use(auth.connect(basic))
   .use(unblocker(unblockerConfig))
   .use('/', express.static(__dirname + '/public'))
-  .get('/', (req, res) => res.send(`Hello from express - ${req.user}!`))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+/*app.get("/no-js", function(req, res) {
+    // grab the "url" parameter from the querystring
+    var site = querystring.parse(url.parse(req.url).query).url;
+    // and redirect the user to /proxy/url
+    res.redirect(unblockerConfig.prefix + site);
+});*/
 
 /*const express = require('express');
 const auth = require('http-auth');
