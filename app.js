daab626a6ea69354b1860a5276c49baca2048dc8
RajasG <rajasmgandhi@gmail.com>
2019-11-04 07:42:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ var unblockerConfig = {
     prefix: '/proxy/'
 };
 
-
 app
   .use(auth.connect(basic))
   .use(unblocker(unblockerConfig))
@@ -45,33 +44,3 @@ app.get("/no-js", function(req, res) {
 
 //app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 module.exports = app;
-
-/*const express = require('express');
-const auth = require('http-auth');
-
-require('dotenv').config();
-
-const basic = auth.basic({
-        realm: "Private Area"
-    }, (username, password, callback) => {
-        callback(username === process.env.USER_NAME && password === process.env.USER_PASSW);
-    }
-);
-
-basic.on('fail', (result, req) => {
-  console.log(`User authentication failed: ${result.user}`);
-});
-
-basic.on('error', (error, req) => {
-  console.log(`Authentication error: ${error.code + " - " + error.message}`);
-});
-
-const PORT = process.env.PORT || 5000;
- 
-express()
-  .use(auth.connect(basic))
-  .get('/', (req, res) => res.send(`Hello from express - ${req.user}!`))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));*/
-
-
-
