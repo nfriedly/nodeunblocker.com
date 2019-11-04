8d9a79585894f0d04509c408a2f135516daff850
RajasG <rajasmgandhi@gmail.com>
2019-11-04 07:51:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -25,13 +25,57 @@ const PORT = process.env.PORT || 5000;
 
 var app = express();
 
-var unblockerConfig = {
-    prefix: '/proxy/'
-};
+var config = {
+    prefix: '/proxy/',
+    host: null,
+    requestMiddleware: [],
+    responseMiddleware: [],
+    standardMiddleware: false,  // disables all built-in middleware
+    processContentTypes: [
+        'text/html',
+        'application/xml+xhtml',
+        'application/xhtml+xml'
+    ]
+}
+ 
+var host = unblocker.host(config);
+var referer = unblocker.referer(config);
+var cookies = unblocker.cookies(config);
+var hsts = unblocker.hsts(config);
+var hpkp = unblocker.hpkp(config);
+var csp = unblocker.csp(config);
+var redirects = unblocker.redirects(config);
+var decompress = unblocker.decompress(config);
+var charsets = unblocker.charsets(config);
+var urlPrefixer = unblocker.urlPrefixer(config);
+var metaRobots = unblocker.metaRobots(config);
+var contentLength = unblocker.contentLength(config);
+ 
+config.requestMiddleware = [
+    host,
+    referer,
+    decompress.handleRequest,
+    cookies.handleRequest
+    // custom requestMiddleware here
+];
+ 
+config.responseMiddleware = [
+    hsts,
+    hpkp,
+    csp,
+    redirects,
+    decompress.handleResponse,
+    charsets,
+    urlPrefixer,
+    cookies.handleResponse,
+    metaRobots,
+    // custom responseMiddleware here
+    contentLength
+];
 
 app
   .use(auth.connect(basic))
-  .use(unblocker(unblockerConfig))
+  .use(unblocker(config))
   .use('/', express.static(__dirname + '/public'))
   .get('/', (req, res) => res.send(`Welcome to private area - ${req.user}!`));
 
@@ -39,7 +83,7 @@ app.get("/no-js", function(req, res) {
     // grab the "url" parameter from the querystring
     var site = querystring.parse(url.parse(req.url).query).url;
     // and redirect the user to /proxy/url
-    res.redirect(unblockerConfig.prefix + site);
+    res.redirect(config.prefix + site);
 });
 
 //app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
